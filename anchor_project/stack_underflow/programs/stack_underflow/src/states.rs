9b36ea27264646d7ef4aaa4fe7d3e50c1528de0f
Christone007 <exellentemy@gmail.com>
2025-11-15 14:05:31 +0000

    Set initial constants

---
+++ anchor_project/stack_underflow/programs/stack_underflow/src/states.rs
@@ -0,0 +1,8 @@
+use anchor_lang::prelude::*;
+
+pub const QUESTION_TOPIC_LENGTH: usize = 32;
+pub const QUESTION_BODY_LENGTH: usize = 200;
+pub const ANSWER_LENGTH: usize = 300;
+
+pub const QUESTION_SEED: &str = "QUESTION";
+pub const ANSWER_SEED: &str = "ANSWER";
